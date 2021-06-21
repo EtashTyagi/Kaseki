9cabc758614bdf0ea8ead61c4626892237ffd689
pranaybhardwaj554 <55682223+pranaybhardwaj554@users.noreply.github.com>
2021-06-21 17:05:41 +0530

    .

--- app/src/main/java/com/example/kaseki/MainActivity.java
+++ app/src/main/java/com/example/kaseki/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.kaseki;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.widget.SeekBar;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -12,5 +15,9 @@ public class MainActivity extends AppCompatActivity {
         Utils.changeStatusBarColor(     this, R.color.transparent_black);
         Utils.changeNavBarColor(this, R.color.black);
         setContentView(R.layout.activity_main);
+
+        // For just checking
+        SeekBar bar = findViewById(R.id.seekBar);
+        bar.setProgressTintList(ColorStateList.valueOf(Color.WHITE));
     }
 }
